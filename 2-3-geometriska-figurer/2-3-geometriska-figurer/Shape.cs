54623e67b85a1c4049e5b9e7d985268176fa4ab3
kn222gn <kn222gn@student.lnu.se>
2014-10-08 15:12:39 +0200

    nu har jag börjat med klassen Shape

---
+++ 2-3-geometriska-figurer/2-3-geometriska-figurer/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_3_geometriska_figurer
+{
+    class Shape
+    {
+        private double = _length;
+        private double = _width;
+
+        public double area
+        {
+            get;
+        }
+
+        public double Length
+        {
+            get;
+            set;
+        }
+        public double Perimeter
+        {
+            get;
+        }
+
+        public double Width
+        {
+            get;
+            set;
+        }
+
+        protected Shape(double length, double width)
+        {
+
+        }
+
+        public string ToString()
+        {
+
+        }
+
+    }
+}
